1f16467e5dc14754a38d8efc466d705ac287d308
Bhargav7032 <130824195+Bhargav7032@users.noreply.github.com>
2024-05-02 15:39:50 +0530

    Add files via upload

---
+++ whileLoop/WhileExample2.java
@@ -0,0 +1,9 @@
+package whileLoop;
+public class WhileExample2 {    
+public static void main(String[] args) {   
+ // setting the infinite while loop by passing true to the condition  
+    while(true){    
+        System.out.println("infinitive while loop");    
+    }    
+}    
+}    
